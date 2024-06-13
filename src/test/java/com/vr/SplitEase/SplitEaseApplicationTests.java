3f26d9288118aa481c7a91f5a46054c60affc447
vasu.aggarwal <vasu.aggarwal@extramarks.com>
2024-06-13 16:14:56 +0530

    first commit

---
+++ src/test/java/com/vr/SplitEase/SplitEaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vr.SplitEase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitEaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
