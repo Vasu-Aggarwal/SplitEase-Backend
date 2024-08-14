c3a4cc0b39cc25475cc996b9a10c3fbeecaa92e5
vasu.aggarwal <vasu.aggarwal@extramarks.com>
2024-08-14 18:16:02 +0530

    Modifying case for percentage in addTransaction API

--- src/main/java/com/vr/SplitEase/service/impl/TransactionServiceImpl.java
+++ src/main/java/com/vr/SplitEase/service/impl/TransactionServiceImpl.java
@@ -80,6 +80,31 @@ public class TransactionServiceImpl implements TransactionService {
             transaction.setCategory(category);
             transaction = transactionRepository.save(transaction);
 
+            //check if transaction is split using PERCENTAGE.
+            // If percentage then, calculate the amount for each user and assign to that
+            if (addTransactionRequest.getSplitBy().equals(SplitBy.PERCENTAGE)){
+                // Get the map containing user IDs and their corresponding percentages
+                Map<String, Double> updateTransactionAmount = addTransactionRequest.getUsersInvolved();
+
+                // Get the total amount from the request
+                double totalAmount = addTransactionRequest.getAmount();
+
+                // Iterate over the map and update the amounts
+                for (Map.Entry<String, Double> entry : updateTransactionAmount.entrySet()) {
+                    String userId = entry.getKey();
+                    Double percentage = entry.getValue();
+
+                    // Calculate the amount for this user based on the percentage
+                    double calculatedAmount = (percentage / 100) * totalAmount;
+
+                    // Update the map with the calculated amount
+                    updateTransactionAmount.put(userId, calculatedAmount);
+                }
+
+                // If you need to update the request with the new map
+                addTransactionRequest.setUsersInvolved(updateTransactionAmount);
+            }
+
             //check the total amount set to all the users must be equal to the transaction amount
             Double amount = 0.0;
             for (Map.Entry<String, Double> entry : addTransactionRequest.getUsersInvolved().entrySet()) {
