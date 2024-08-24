d20d9eac351778f90a6eb93bf4f2a672dd020af4
Vasu Aggarwal <svasu.sv95@gmail.com>
2024-08-24 20:10:20 +0530

    production setup

--- src/main/java/com/vr/SplitEase/dto/request/GptRequest.java
+++ src/main/java/com/vr/SplitEase/dto/request/GptRequest.java
@@ -22,7 +22,7 @@ public class GptRequest {
         this.messages = new ArrayList<>();
         this.messages.add(new GptMessage("system", "User will provide a sentence might consist one word or more than one word. Currently the user belong from INDIA only. Your job is to always provide a single word response which is best suited category from the text provided. Given below is the category list, use this list only to detect the category if not found then return general:\\nGeneral\\nGames\\nMovies\\nMusic\\nSports\\nOther_Entertainment\\nGroceries\\nDining Out\\nOther_Food\\nLiquor\\nRent\\nMortgage\\nHousehold supplies\\nFurniture\\nMaintenance\\nOther_Home\\nPets\\nServices\\nElectronics\\nInsurance\\nClothing\\nGifts\\nMedical expenses\\nOther_Life\\nTaxes\\nEducation\\nChildcare\\nParking\\nCar\\nBus/train\\nGas/fuel\\nOther_Transportation\\nPlane\\nTaxi\\nHotel\\nElectricity\\nHeat/gas\\nWater\\nTV/phone/internet\\nOther_Utilities\\nCleaning"));
         this.messages.add(new GptMessage("user", prompt));
-        this.max_tokens = 1;
+        this.max_tokens = 3;
         this.n = 1;
     }
 }
